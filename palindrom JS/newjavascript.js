5bd7bbe6b01d2e9647ee075733cbb0774ccea299
Shevchenko Maria <shevchenko.maria@inbox.ru>
2017-10-06 19:31:51 +0200

    Palindrom

--- palindrom JS/newjavascript.js
+++ palindrom JS/newjavascript.js
@@ -1,10 +1,9 @@
 var a1 = "A car,a man, a maraca";
-var a2 = "Lev1s";
+var a2 = "Levels";
 var a3 = "Level";
 
 function P(a) {
     return RegExp(a.match(/\w/g).reverse().join('\\W*'), 'i').test(a);
-
 }
 
 console.log(P(a1));
